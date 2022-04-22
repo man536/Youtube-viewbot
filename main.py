de44e647ec6860a8d9c0303d4a37d597eb3385a0
man536 <103523585+man536@users.noreply.github.com>
2022-04-22 14:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ def scrape_proxies():
 
 def load_url(ua, sleeptime, proxy):
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
     options.add_argument('--proxy-server=%s' % (proxy['ip'] + ':' + proxy['port']))
     options.add_argument('user-agent=%s' % ua.random)
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
